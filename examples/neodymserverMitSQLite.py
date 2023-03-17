51bed4a2161b22bb7c6bf5b079dc4f4e0ebab010
schlechteswetter <107403861+schlechteswetter@users.noreply.github.com>
2023-03-17 18:40:11 +0100

    neodymserver mit SQLite hinzugefügt
    kann im moment nur am anfang alle geräte aus datenbank lesen

---
+++ examples/neodymserverMitSQLite.py
@@ -0,0 +1,113 @@
+import asyncio
+import websockets
+import json
+import sqlite3
+
+# NEODYM Server Beta V2 mit SQLite für Devices Speicher
+# akzeptiert Websocket Verbindungen und leitet Befehle weiter
+# und speichert geräte
+
+
+try:
+   connection = sqlite3.connect("devices.db")
+   cursor = connection.cursor()
+   print("erfolgreich mit Datenbank verbunden")
+   try:
+      cursor.execute("CREATE TABLE devices (id INTEGER PRIMARY KEY, password TEXT NOT NULL)")
+      cursor.execute("INSERT INTO devices (id, password) VALUES (0, '')")
+      cursor.execute("INSERT INTO devices (id, password) VALUES (1, '1234')")
+      cursor.execute("INSERT INTO devices (id, password) VALUES (2, '5678')")
+   except:
+      print("Datenbank Anfangsstand existiert bereits")
+
+except:
+   print("keine Datenbankverbindung möglich")
+
+DBresult = cursor.execute("SELECT * FROM devices").fetchall()
+devices = []
+
+for device in DBresult:
+   devices.append({
+    "id": device[0],
+    "password": device[1],
+    "websocket": ""
+   })
+
+print(devices)
+
+def getDeviceById(id):
+   for element in devices:
+      if((element["id"] == id) ):
+         return element
+      
+def executeCommand(device, message):
+   print("führe Befehl aus: " + str(message))
+
+   # wenn Password ändern:
+   if(message["command"] == "changePassword"):
+      getDeviceById(device)["password"] = message["value"]
+      return '{"success": true}'
+
+
+async def handle(websocket, path):
+   
+   global devices
+   connectedDevice = 0
+   data = await websocket.recv()
+   jsonresult = json.loads(data)
+
+   try:
+      element = getDeviceById(jsonresult["id"])
+      if(element["password"] == jsonresult["password"]):
+         connectedDevice = jsonresult["id"]
+         reply = '{"success" true, "id": '+ str(connectedDevice) + '}'
+         element["websocket"] = websocket
+         
+      else:
+         reply = '{"success" false}'
+   except:
+      reply = '{"success" false}'
+
+   
+   # for element in devices:
+   #    if((element["id"] == jsonresult["id"]) & (element["password"] == jsonresult["password"])):
+   #       reply = '{"success" true, "id": '+ str(jsonresult["id"]) + '}'
+   #       element["websocket"] = websocket
+   
+
+   # if((jsonresult["id"] == 1) & (jsonresult["password"] == "1234")):
+   #    reply = '{"success" true, "id": 1}'
+   #    devices[1] = websocket
+
+   # elif((jsonresult["id"] == 2) & (jsonresult["password"] == "5678")):
+   #    reply = '{"success" true, "id": 2}'
+   #    devices[2] = websocket
+   #    print("Lampe verbunden")
+
+   # else:
+   #    reply = '{"success" false, "id": 0}'
+
+   # alle angemeldeten Geräte ausgeben
+   print("\n angemeldete Geräte:")
+   for device in devices:
+      print(device)   
+      
+   await websocket.send(reply)
+
+   async for message in websocket:
+      decoded = json.loads(message)
+      try:
+         if(decoded["id"] == 0):
+            await getDeviceById(connectedDevice)["websocket"].send(executeCommand(connectedDevice, decoded))
+         else:
+            await getDeviceById(decoded["id"])["websocket"].send(message)
+        
+      except:
+         print("fehler")
+
+
+
+start_server = websockets.serve(handle, "localhost", 8000)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
